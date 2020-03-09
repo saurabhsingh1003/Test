c936577aa4f549f48a7bb2f48609f78bfa3e3b70
saurabhsingh1003 <61385347+saurabhsingh1003@users.noreply.github.com>
2020-03-09 22:56:38 +0530

    added home and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUSComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUSComponent
   ],
   imports: [
     BrowserModule,
